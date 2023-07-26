bd23d7a67ac28688994e1e86eec764904eda4f31
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-26 10:45:58 +0300

    Register models in admin oanel. Modified interface of admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Tag, Task
+
+
+@admin.register(Task)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_datetime", "deadline_datetime", "is_done"]
+    list_filter = ["created_datetime"]
+    search_fields = ["content"]
+
+
+admin.site.register(Tag)
