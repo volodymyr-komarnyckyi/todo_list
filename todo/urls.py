13f514cbaae3bc6995323abb496691a83aaa64e3
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-26 11:08:43 +0300

    Create home page for todo list, add link to urls.py, inherited index.html from base.html. Create navbar

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from todo.views import index
+
+urlpatterns = [
+    path("", index, name="index"),
+]
+
+app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.models import Tag, Task
+
+
+@login_required
+def index(request):
+    num_tags = Tag.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tags": num_tags,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "todo/index.html", context=context)
